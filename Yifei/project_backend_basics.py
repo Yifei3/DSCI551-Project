bb394c8983e872d1003b64d2cbdcb5f912f37481
Yifei3 <yifei142857@gmail.com>
2024-03-16 00:25:36 -0700

    basic search for student_id

---
+++ Yifei/project_backend_basics.py
@@ -0,0 +1,62 @@
+# IMPORT LIBRARIES
+import sys
+from sqlalchemy import create_engine
+import pymysql
+import pandas as pd
+
+DATABASE_CONNECTION_PARAMS = {
+    # 'host': 'ec2-3-16-11-188.us-east-2.compute.amazonaws.com',
+    'host': 'localhost',
+    'user': 'dsci551',
+    'password': 'Dsci-551',
+    'database': 'CINEMA',
+}
+
+def hash_database(student_id):
+    if student_id % 100 <= 50 or student_id % 100 == 0:
+        return 'student1'
+    else:
+        return 'student2'
+
+def get_info_by_student_id(student_id):
+    """
+    Fetches info of a student that has the specified student_id.
+    INPUT: student_id (int)
+    RETURN: info of that student
+    
+    Sample Terminal Command:python3 project_backend_basics.py 13
+    Expected Sample Output: 
+    """
+
+    DATABASE_CONNECTION_PARAMS['database'] = hash_database(student_id)
+    connection = pymysql.connect(**DATABASE_CONNECTION_PARAMS)
+
+    cursor = connection.cursor()
+
+    query = '''
+            SELECT * FROM students s
+            WHERE s.student_id = %s
+            '''
+    cursor.execute(query, student_id)
+    
+    rows = cursor.fetchall()
+
+    if cursor:
+        cursor.close()
+    if connection:
+        connection.close()
+    return rows[0]
+
+
+# Use the below main method to test your code
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python project_backend_basics.py '<student_id>'")
+        sys.exit(1)
+    student_id = int(sys.argv[1])
+    info = get_info_by_student_id(student_id)
+    if info:
+        print(f"Information of student with student_id {student_id}: {info}")
+    else:
+        print(f"No info found for student with student_id {student_id}.")
+
