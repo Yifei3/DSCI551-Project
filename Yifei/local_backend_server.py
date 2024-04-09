d2114c788d0926b8a6454e37a11333c27854a29b
Yifei3 <yifei142857@gmail.com>
2024-04-08 21:40:57 -0700

    student_id is a pass-in argument now

--- Yifei/local_backend_server.py
+++ Yifei/local_backend_server.py
@@ -1,4 +1,5 @@
 import paramiko
+import sys
 
 # SSH connection setup
 ssh = paramiko.SSHClient()
@@ -7,7 +8,8 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 private_key_path = 'dsci551-sp24.pem'
 ssh.connect('ec2-3-14-71-47.us-east-2.compute.amazonaws.com', username ='ubuntu', key_filename = private_key_path)
 
-stdin, stdout, stderr = ssh.exec_command('python3 /home/ubuntu/Project/project_backend_basics.py 1')
+student_id = sys.argv[1]
+stdin, stdout, stderr = ssh.exec_command(f'python3 /home/ubuntu/Project/project_backend_basics.py {student_id}')
 
 #Reading output (optional)
 output = stdout.read().decode('utf-8')
