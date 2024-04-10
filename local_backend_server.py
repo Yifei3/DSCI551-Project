837081047a972784dbbf8ce0acb7ead8bff25d99
NEKOM1M1 <carolinechang929@gmail.com>
2024-04-09 19:56:23 -0700

    1

---
+++ local_backend_server.py
@@ -0,0 +1,77 @@
+import paramiko
+import sys
+import json
+
+class sql_crud:
+    @staticmethod
+    def get_student_info(ssh, student_id):
+        stdin, stdout, stderr = ssh.exec_command(f'python3 /home/ubuntu/Project/project_backend_basics.py get_student_info {student_id}')
+        output = stdout.read().decode('utf-8')
+        print(output)
+
+    @staticmethod
+    def get_professor_info(ssh, professor_id):
+        stdin, stdout, stderr = ssh.exec_command(f'python3 /home/ubuntu/Project/project_backend_basics.py get_professor_info {professor_id}')
+        output = stdout.read().decode('utf-8')
+        print(output)
+
+    @staticmethod
+    def get_course_info(ssh, course_id):
+        stdin, stdout, stderr = ssh.exec_command(f'python3 /home/ubuntu/Project/project_backend_basics.py get_course_info {course_id}')
+        output = stdout.read().decode('utf-8')
+        print(output)
+
+    @staticmethod
+    def enroll_student(ssh, student_info): # {student_id, student_name, gender, email, department_id, gpa}
+        student_info_json = json.dumps(student_info)
+        # print(student_info_json)
+        stdin, stdout, stderr = ssh.exec_command(f'python3 /home/ubuntu/Project/project_backend_basics.py enroll_student \'{student_info_json}\'')
+        output = stdout.read().decode('utf-8')
+        print(output)
+
+    @staticmethod
+    def withdraw_student(ssh, student_id):
+        ...
+
+    @staticmethod
+    def student_enroll_course(ssh, student_id, course_id):
+        ...
+
+    @staticmethod
+    def student_withdraw_course(ssh, student_id, course_id):
+        ...
+
+    @staticmethod
+    def search_students_by_gpa(ssh, gpa_range): # [min, max]
+        ...
+
+    @staticmethod
+    def search_students_from_professor_id(ssh, professor_id):
+        ...
+
+    @staticmethod
+    def search_classmates_from_student_id(ssh, student_id):
+        ...
+
+    @staticmethod
+    def modify_student_info(ssh, student_id, student_info):
+        ...
+
+# SSH connection setup
+def connection_setup():
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    private_key_path = 'dsci551-sp24.pem'
+    ssh.connect('ec2-18-217-182-18.us-east-2.compute.amazonaws.com', username ='ubuntu', key_filename = private_key_path)
+    return ssh
+
+
+if __name__ == "__main__":
+    json_str = sys.argv[1]
+    input = json.loads(json_str)
+    method_name, data = next(iter(input.items()))
+    obj = sql_crud()
+    ssh = connection_setup()
+    getattr(obj, method_name)(ssh, data)
+    ssh.close()
--- main.py
+++ main.py
@@ -33,15 +33,16 @@ def index():
 
 @app.route('/get_student_info', methods=['POST'])
 def get_info_by_student_id():
-    try:
-        data = request.form['data']
-        ssh = initialize_ssh()
-        command = f'python3 /home/ubuntu/Project/project_backend_basics.py get_student_info {data}'
-        stdin, stdout, stderr = ssh.exec_command(command)
-        output = stdout.read().decode('utf-8')
-        print("output", output)
-        ssh.close()
+
+    data = request.form['data']
+    ssh = initialize_ssh()
+    command = f'python3 /home/ubuntu/Project/project_backend_basics.py get_student_info {data}'
+    stdin, stdout, stderr = ssh.exec_command(command)
+    output = stdout.read().decode('utf-8')
+    print("output", output)
+    ssh.close()
         # print(type(output))
+    try:
         info = eval(output)
         # print(type(info))
         student_info = {
@@ -55,138 +56,112 @@ def get_info_by_student_id():
         print(student_info)
         return render_template('student.html', result=student_info)
     except Exception as e:
-        return render_template('error.html')
+        return render_template('error.html',result=output)
 
 @app.route('/get_professor_info', methods=['POST'])
 def get_professor_info():
+    data = request.form['data']
+    ssh = initialize_ssh()
+    command = f'python3 /home/ubuntu/Project/project_backend_basics.py get_professor_info {data}'
+    stdin, stdout, stderr = ssh.exec_command(command)
+    output = stdout.read().decode('utf-8')
+    print("output", output)
+    ssh.close()
     try:
-        data = request.form['data']
-        ssh = initialize_ssh()
-        command = f'python3 /home/ubuntu/Project/project_backend_basics.py get_student_info {data}'
-        stdin, stdout, stderr = ssh.exec_command(command)
-        output = stdout.read().decode('utf-8')
-        print("output", output)
-        ssh.close()
-        # print(type(output))
         info = eval(output)
         # print(type(info))
-        student_info = {
-            'student_id': info['student_id'],
-            'student_name': info['student_name'],
+        professor_info = {
+            'professor_id': info['professor_id'],
+            'professor_name': info['name'],
             'gender': info['gender'],
             'email': info['email'],
             'department_id': info['department_id'],
-            'gpa': info['gpa']
+            'salary': info['salary']
         }
-        print(student_info)
-        return render_template('student.html', result=student_info)
+        print(professor_info)
+        return render_template('professor.html', result=professor_info)
     except Exception as e:
-        return render_template('error.html')
+        return render_template('error.html',result=output)
 
 @app.route('/get_course_info', methods=['POST'])
 def get_course_info():
+    data = request.form['data']
+    ssh = initialize_ssh()
+    command = f'python3 /home/ubuntu/Project/project_backend_basics.py get_course_info {data}'
+    stdin, stdout, stderr = ssh.exec_command(command)
+    output = stdout.read().decode('utf-8')
+    print("output", output)
+    ssh.close()
     try:
-        data = request.form['data']
-        ssh = initialize_ssh()
-        command = f'python3 /home/ubuntu/Project/project_backend_basics.py get_student_info {data}'
-        stdin, stdout, stderr = ssh.exec_command(command)
-        output = stdout.read().decode('utf-8')
-        print("output", output)
-        ssh.close()
-        # print(type(output))
         info = eval(output)
         # print(type(info))
-        student_info = {
-            'student_id': info['student_id'],
-            'student_name': info['student_name'],
-            'gender': info['gender'],
-            'email': info['email'],
+        course_info = {
+            'course_id': info['course_id'],
+            'prof_id': info['prof_id'],
+            'name': info['name'],
             'department_id': info['department_id'],
-            'gpa': info['gpa']
+            'schedule_time': info['schedule_time'],
+            'seat_available': info['seat_available'],
+            'description': info['description']
         }
-        print(student_info)
-        return render_template('student.html', result=student_info)
+        print(course_info)
+        return render_template('course.html', result=course_info)
     except Exception as e:
-        return render_template('error.html')
+        return render_template('error.html', result=output)
 
 @app.route('/search_students_by_gpa', methods=['POST'])
 def search_students_by_gpa():
-    try:
-        data = request.form['data']
+        min_gpa = request.form['min_gpa']
+        max_gpa = request.form['max_gpa']
+        data = min_gpa + "," + max_gpa
+        print(data)
         ssh = initialize_ssh()
-        command = f'python3 /home/ubuntu/Project/project_backend_basics.py get_student_info {data}'
+        command = f'python3 /home/ubuntu/Project/project_backend_basics.py search_students_by_gpa {data}'
         stdin, stdout, stderr = ssh.exec_command(command)
         output = stdout.read().decode('utf-8')
         print("output", output)
         ssh.close()
-        # print(type(output))
-        info = eval(output)
-        # print(type(info))
-        student_info = {
-            'student_id': info['student_id'],
-            'student_name': info['student_name'],
-            'gender': info['gender'],
-            'email': info['email'],
-            'department_id': info['department_id'],
-            'gpa': info['gpa']
-        }
-        print(student_info)
-        return render_template('student.html', result=student_info)
-    except Exception as e:
-        return render_template('error.html')
+        print(type(output))
+        try:
+            data_list = eval(output.strip())
+            print(type(data_list),data_list)
+            return render_template('student_list.html', result=data_list)
+        except Exception as e:
+            return render_template('error.html',result="gpa value needs to be a float range from 0 to 4.00")
 
 @app.route('/search_students_from_professor_id', methods=['POST'])
 def search_students_from_professor_id():
+    data = request.form['data']
+    ssh = initialize_ssh()
+    command = f'python3 /home/ubuntu/Project/project_backend_basics.py search_students_from_professor_id {data}'
+    stdin, stdout, stderr = ssh.exec_command(command)
+    output = stdout.read().decode('utf-8')
+    print("output", output)
+    ssh.close()
+    print(type(output))
     try:
-        data = request.form['data']
-        ssh = initialize_ssh()
-        command = f'python3 /home/ubuntu/Project/project_backend_basics.py get_student_info {data}'
-        stdin, stdout, stderr = ssh.exec_command(command)
-        output = stdout.read().decode('utf-8')
-        print("output", output)
-        ssh.close()
-        # print(type(output))
-        info = eval(output)
-        # print(type(info))
-        student_info = {
-            'student_id': info['student_id'],
-            'student_name': info['student_name'],
-            'gender': info['gender'],
-            'email': info['email'],
-            'department_id': info['department_id'],
-            'gpa': info['gpa']
-        }
-        print(student_info)
-        return render_template('student.html', result=student_info)
+        data_list = eval(output.strip())
+        print(type(data_list), data_list)
+        return render_template('student_list.html', result=data_list)
     except Exception as e:
-        return render_template('error.html')
+        return render_template('error.html', result=output)
 
 @app.route('/search_classmates_from_student_id', methods=['POST'])
 def search_classmates_from_student_id():
+    data = request.form['data']
+    ssh = initialize_ssh()
+    command = f'python3 /home/ubuntu/Project/project_backend_basics.py search_classmates_from_student_id {data}'
+    stdin, stdout, stderr = ssh.exec_command(command)
+    output = stdout.read().decode('utf-8')
+    print("output", output)
+    ssh.close()
+    print(type(output))
     try:
-        data = request.form['data']
-        ssh = initialize_ssh()
-        command = f'python3 /home/ubuntu/Project/project_backend_basics.py get_student_info {data}'
-        stdin, stdout, stderr = ssh.exec_command(command)
-        output = stdout.read().decode('utf-8')
-        print("output", output)
-        ssh.close()
-        # print(type(output))
-        info = eval(output)
-        # print(type(info))
-        student_info = {
-            'student_id': info['student_id'],
-            'student_name': info['student_name'],
-            'gender': info['gender'],
-            'email': info['email'],
-            'department_id': info['department_id'],
-            'gpa': info['gpa']
-        }
-        print(student_info)
-        return render_template('student.html', result=student_info)
+        data_list = eval(output.strip())
+        print(type(data_list), data_list)
+        return render_template('student_list.html', result=data_list)
     except Exception as e:
-        return render_template('error.html')
-
+        return render_template('error.html', result=output)
 
 
 
@@ -196,13 +171,11 @@ def search_classmates_from_student_id():
 @app.route('/enroll_student', methods=['POST'])
 def enroll_student():
     try:
-
         student_name = request.form['student_name']
         gender = request.form['gender']
         email = request.form['email']
         department_id = request.form['department_id']
         gpa = request.form['gpa']
-
         data = {
             'name': student_name,
             'gender': gender,
@@ -210,27 +183,63 @@ def enroll_student():
             'department_id': int(department_id),
             'gpa': float(gpa)
         }
-
-        print(type(data),data)
+        # print(type(data),data)
         data = json.dumps(data)
-        print(type(data), data)
+        # print(type(data), data)
 
         ssh = initialize_ssh()
         command = f'python3 /home/ubuntu/Project/project_backend_basics.py enroll_student \'{data}\''
-
-
-# python3 / home / ubuntu / Project / project_backend_basics.py enroll_student '{"name": "jenny", "gender": "female", "email": "jenfsd@gmail.com", "department_id": 3, "gpa": 3.67}'
-
-        print(command)
+        # print(command)
         stdin, stdout, stderr = ssh.exec_command(command)
         output = stdout.read().decode('utf-8')
         print("output", output)
         ssh.close()
 
         return render_template('result.html', result=output)
+    except Exception as e:
+        return render_template('result.html', result="Failed")
+
+@app.route('/student_enroll_course', methods=['POST'])
+def student_enroll_course():
+    try:
+        student_id = request.form['student_id']
+        course_id = request.form['course_id']
+
+        data=str(course_id) + ',' + str(student_id)
+        print(data)
+
+        ssh = initialize_ssh()
+
+        command = f'python3 /home/ubuntu/Project/project_backend_basics.py student_enroll_course {data}'
+        print(command)
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.read().decode('utf-8')
+        print("output", output)
+        ssh.close()
+        return render_template('result.html', result=output)
 
     except Exception as e:
         return render_template('result.html', result="Failed")
+
+
+#withdraw
+
+@app.route('/withdraw_student', methods=['POST'])
+def withdraw_student():
+    try:
+        data = request.form['data']
+        ssh = initialize_ssh()
+        command = f'python3 /home/ubuntu/Project/project_backend_basics.py withdraw_student {data}'
+        print(command)
+        stdin, stdout, stderr = ssh.exec_command(command)
+        output = stdout.read().decode('utf-8')
+        print("output", output)
+        ssh.close()
+        return render_template('result.html', result=output)
+    except Exception as e:
+        return render_template('result.html', result="Failed")
+
+
 
 
 if __name__ == '__main__':
